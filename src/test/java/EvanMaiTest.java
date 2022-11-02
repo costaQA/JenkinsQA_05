85d3ff9ee81887dc4a31c0e9de609b92e51fc349
Evan Mai <mai.evan@yahoo.com>
2022-11-01 20:25:25 -0700

    add couple tests (#22)

--- src/test/java/EvanMaiTest.java
+++ src/test/java/EvanMaiTest.java
@@ -13,6 +13,28 @@ public class EvanMaiTest extends BaseTest {
     }
     @Test
     public void test2() {
-
+        getDriver().get("https://katalon-demo-cura.herokuapp.com/");
+        Assert.assertEquals(getDriver().getTitle(),"CURA Healthcare Service");
+    }
+    @Test
+    public void test3() {
+        getDriver().get("https://katalon-demo-cura.herokuapp.com/");
+        WebElement makeAppoiment = getDriver().findElement(By.id("btn-make-appointment"));
+        makeAppoiment.click();
+        getDriver().findElement(By.name("username")).sendKeys("Evan");
+        getDriver().findElement(By.name("password")).sendKeys("123456789");
+        getDriver().findElement(By.className("btn-default")).click();
+        boolean currentUrl = getDriver().getCurrentUrl().equals("https://katalon-demo-cura.herokuapp.com/profile.php#login");
+        Assert.assertTrue(currentUrl);
+    }
+    @Test
+    public void test4() {
+        getDriver().get("https://katalon-demo-cura.herokuapp.com/");
+        Assert.assertTrue(getDriver().findElement(By.xpath("/html/body/footer/div/div/div/ul[1]/li[1]")).isDisplayed());
+    }
+    @Test
+    public void test5() {
+        getDriver().get("https://katalon-demo-cura.herokuapp.com/");
+        Assert.assertTrue(getDriver().findElement(By.xpath("/html/body/footer/div/div/div/ul[1]/li[1]")).isDisplayed());
     }
 }
