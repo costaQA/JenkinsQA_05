63b71957eb1926d556c0b85a97a3c18064581344
Stanislaw1349 <92247013+Stanislaw1349@users.noreply.github.com>
2022-11-02 23:49:55 +0300

    Create new class, add first test (#53)

---
+++ src/test/java/GroupCubsTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class GroupCubsTest extends BaseTest {
+
+    @Test
+    public void testFelix_IX() {
+        getDriver().get("https://habr.com/ru/all/");
+
+        String query ="приоритет тест-кейса в TestNG";
+        getDriver().findElement(By.xpath("//a[@data-test-id='search-button']")).click();
+        getDriver().findElement(By.className("tm-input-text-decorated__input")).sendKeys(query + "\n");
+        getDriver().findElement(By.xpath("//article[@id='588466']/div[1]/h2")).click();
+        WebElement actualRes = getDriver().findElement(By.xpath("//h1[@data-test-id='articleTitle']"));
+        Assert.assertEquals(actualRes.getText(), "Как установить приоритет тест-кейса в TestNG с помощью Selenium");
+    }
+}
