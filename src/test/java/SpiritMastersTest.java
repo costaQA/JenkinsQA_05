099b834c16c9b9a5f91d558a7bbb15a6a3f87870
Pavel <63100397+kartavenkopavel@users.noreply.github.com>
2022-11-02 19:55:14 +0300

    added category link tests (#32)

--- src/test/java/SpiritMastersTest.java
+++ src/test/java/SpiritMastersTest.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.List;
+
 public class SpiritMastersTest extends BaseTest {
 
     @Test
@@ -20,5 +22,52 @@ public class SpiritMastersTest extends BaseTest {
         getDriver().findElement(By.xpath("//div[@class='card-body']/h5")).click();
 
         Assert.assertEquals(getDriver().findElement(By.className("main-header")).getText(), "Elements");
+    }
+    private WebElement findCard_PK(int index) {
+        getDriver().get("https://demoqa.com/");
+        List<WebElement> category = getDriver().findElements(By.className("card"));
+        return category.get(index);
+    }
+
+    @Test
+    public void test_PK_RedirectToElementsTab(){
+        findCard_PK(0).click();
+        String actualTextElements = getDriver().findElement(By.className("main-header")).getText();
+        Assert.assertEquals(actualTextElements, "Elements");
+    }
+
+    @Test
+    public void test_PK_RedirectToFormsTab() {
+        findCard_PK(1).click();
+        String actualTextForms = getDriver().findElement(By.className("main-header")).getText();
+        Assert.assertEquals(actualTextForms, "Forms");
+    }
+
+    @Test
+    public void test_PK_RedirectToAlertsTab(){
+        findCard_PK(2).click();
+        String actualTextAlerts = getDriver().findElement(By.className("main-header")).getText();
+        Assert.assertEquals(actualTextAlerts, "Alerts, Frame & Windows");
+    }
+
+    @Test
+    public void test_PK_RedirectToWidgetsTab(){
+        findCard_PK(3).click();
+        String actualTextWidgets = getDriver().findElement(By.className("main-header")).getText();
+        Assert.assertEquals(actualTextWidgets, "Widgets");
+    }
+
+    @Test
+    public void test_PK_RedirectToInteractionsTab(){
+        findCard_PK(4).click();
+        String actualTextInteractions = getDriver().findElement(By.className("main-header")).getText();
+        Assert.assertEquals(actualTextInteractions, "Interactions");
+    }
+
+    @Test
+    public void test_PK_RedirectToBooksTab(){
+        findCard_PK(5).click();
+        String actualTextBooks = getDriver().findElement(By.className("main-header")).getText();
+        Assert.assertEquals(actualTextBooks, "Book Store");
     }
 }
