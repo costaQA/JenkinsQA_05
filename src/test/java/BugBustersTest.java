73b5352ec6d68173ee734ecc39bda79d79082e1f
Arailym <33895096+ArailymAsar@users.noreply.github.com>
2022-11-02 08:00:32 +0600

    add Arailym test (#9)

---
+++ src/test/java/BugBustersTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class BugBustersTest extends BaseTest {
+
+    @Test
+    public void testArailymSuccessLogIn(){
+        getDriver().get("https://katalon-demo-cura.herokuapp.com/");
+        getDriver().findElement(By.xpath("//body/a/i")).click();
+        getDriver().findElement(By.xpath("//body/nav/ul/li/a[@href = 'profile.php#login']")).click();
+        getDriver().findElement(By.id("txt-username")).sendKeys("John Doe");
+        getDriver().findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+        getDriver().findElement(By.id("btn-login")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h2")).getText(), "Make Appointment");
+
+    }
+}
