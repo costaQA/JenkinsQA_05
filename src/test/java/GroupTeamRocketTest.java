d5f461ebb3008fdd12ef72efa3ee4ba62231cf56
ArtemChumachenko <115054226+ArtemChumachenko@users.noreply.github.com>
2022-11-02 01:58:45 -0400

    new test (#27)

--- src/test/java/GroupTeamRocketTest.java
+++ src/test/java/GroupTeamRocketTest.java
@@ -12,4 +12,12 @@ public class GroupTeamRocketTest extends BaseTest {
         getDriver().findElement(By.xpath("//button[@onclick='addElement()']")).click();
         Assert.assertTrue(getDriver().findElement(By.xpath("//button[@class='added-manually']")).isDisplayed());
     }
+    @Test
+    public void testSwagLabs_LogIn()  {
+        getDriver ().get ("https://www.saucedemo.com");
+        getDriver ().findElement (By.id ("user-name")).sendKeys ("standard_user");
+        getDriver ().findElement (By.id ("password")).sendKeys ("secret_sauce");
+        getDriver ().findElement (By.id ("login-button")).click ();
+        Assert.assertEquals (getDriver ().getCurrentUrl (),"https://www.saucedemo.com/inventory.html");
+    }
 }
