068a6019c340fa995f466d873d7cf08a5d58ec58
Denis Sebrovsky <den.sebrovsky@mail.ru>
2022-11-03 16:28:04 +0300

    SpiritMastersTest | testDSFindTitle() (#91)
    * added tests by DenSebrovsky
    
    * added testDSFindTitle() to SpiritMastersTest

--- src/test/java/SpiritMastersTest.java
+++ src/test/java/SpiritMastersTest.java
@@ -175,12 +175,13 @@ public class SpiritMastersTest extends BaseTest {
     }
 
     @Test
-    public void testDenSebrovskyOpenQABible() throws InterruptedException {
+    public void testDSOpenQABible() throws InterruptedException {
         getDriver().get("https://vladislaveremeev.gitbook.io/qa_bible/");
         Thread.sleep(1500);
         WebElement firstTitle = getDriver().findElement(By.linkText("QA_Bible"));
         Assert.assertEquals(firstTitle.getText(), "QA_Bible");
     }
+
     @Test
     public void testRedirectToSeleniumTrainingTab_PK(){
         getDriver().get("https://demoqa.com/");
@@ -229,4 +230,14 @@ public class SpiritMastersTest extends BaseTest {
 
     }
 
+    @Test
+    public void testDSFindTitle() throws InterruptedException {
+        getDriver().get("https://vladislaveremeev.gitbook.io/qa_bible/");
+        Thread.sleep(1500);
+        getDriver().findElement(By.xpath("//div[4]//a[1]//div[1]")).click();
+        WebElement title = getDriver().findElement(By.xpath(
+                "//div[contains(@class, 'css-901oao r')][contains(text(),'Принципы тестирования')]"));
+        title.click();
+        Assert.assertEquals(title.getText(), "Принципы тестирования");
+    }
 }
