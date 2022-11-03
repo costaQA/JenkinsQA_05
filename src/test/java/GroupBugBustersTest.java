f377b0be7f15868265c04671a00cdecca59b7452
Nadia Ludanik <nadialidt@gmail.com>
2022-11-03 20:19:33 +0300

    groupBugBustersTestSucssesDoctorAppointment (#98)

--- src/test/java/GroupBugBustersTest.java
+++ src/test/java/GroupBugBustersTest.java
@@ -15,4 +15,44 @@ public class GroupBugBustersTest extends BaseTest {
         getDriver().findElement(By.id("btn-login")).click();
         Assert.assertEquals(getDriver().findElement(By.xpath("//h2")).getText(), "Make Appointment");
     }
+
+    @Test
+    public void testNadiaSucssesDoctorAppointment() throws InterruptedException {
+        String expectedResultHospitalName = "Hongkong CURA Healthcare Center";
+        String expectedApplayHospitalReadmission = "Yes";
+        String expectedResultHealthProgram = "Medicaid";
+        String expectedResultVisitDate = "23/11/2022";
+        String expectedResultComment = "";
+
+        getDriver().get("https://katalon-demo-cura.herokuapp.com/");
+        getDriver().findElement(By.xpath("//a[@id='btn-make-appointment']")).click();
+        getDriver().findElement(By.xpath("//input[@id='txt-username']")).sendKeys("John Doe");
+        getDriver().findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+        getDriver().findElement(By.id("btn-login")).click();
+        Thread.sleep(1000);
+        getDriver().findElement(By.id("combo_facility")).click();
+
+        getDriver().findElement(By.xpath("//select[@id='combo_facility']/option" +
+                "[@value='Hongkong CURA Healthcare Center']")).click();
+        //Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//input[@id='chk_hospotal_readmission']")).click();
+        //Thread.sleep(1000);
+        getDriver().findElement(By.xpath("//input[@id='radio_program_medicaid']")).click();
+        getDriver().findElement(By.xpath("//section[@id=\"appointment\"]/div/div/form/div/div/div/div/span" +
+                "[@class='glyphicon glyphicon-calendar']")).click();
+        getDriver().findElement(By.xpath("//body/div/div/table/tbody/tr[4]/td[4]")).click();
+        getDriver().findElement(By.id("btn-book-appointment")).click();
+
+        String actualResulttHospitalName = getDriver().findElement(By.id("facility")).getText();
+        String actualResulttApplayHospitalReadmission = getDriver().findElement(By.id("hospital_readmission")).getText();
+        String actualResultHealthProgram = getDriver().findElement(By.id("program")).getText();
+        String actualResultVisitDate = getDriver().findElement(By.id("visit_date")).getText();
+        String actualResultComment = getDriver().findElement(By.id("comment")).getText();
+
+        Assert.assertEquals(actualResulttHospitalName, expectedResultHospitalName);
+        Assert.assertEquals(actualResulttApplayHospitalReadmission, expectedApplayHospitalReadmission);
+        Assert.assertEquals(actualResultHealthProgram, expectedResultHealthProgram);
+        Assert.assertEquals(actualResultVisitDate, expectedResultVisitDate);
+        Assert.assertEquals(actualResultComment, expectedResultComment);
+    }
 }
