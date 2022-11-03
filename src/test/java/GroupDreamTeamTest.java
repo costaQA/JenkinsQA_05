166be0f45ecda8b2eb6b1ea622c69ace5ae403b4
Elgrueber <89706559+Elgrueber@users.noreply.github.com>
2022-11-03 17:31:18 -0400

    Eg/my test (#119)
    * add test
    
    * new refactor code, added test Elena

--- src/test/java/GroupDreamTeamTest.java
+++ src/test/java/GroupDreamTeamTest.java
@@ -8,19 +8,6 @@ import runner.BaseTest;
 public class GroupDreamTeamTest extends BaseTest {
 
     @Test
-    public void testFirstTest() {
-        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
-        WebElement textInput = getDriver().findElement(By.id("my-text-id"));
-        textInput.clear();
-        textInput.sendKeys("Hey man");
-        WebElement dropdownSelect = getDriver().findElement(By.name("my-select"));
-        Select sel = new Select(dropdownSelect);
-        sel.selectByValue("1");
-        WebElement one = getDriver().findElement(By.xpath("//option[@value='1']"));
-        Assert.assertTrue(one.isSelected());
-    }
-
-    @Test
     public void testDarina() {
         getDriver().get("https://the-internet.herokuapp.com/login");
         WebElement usernameTextField = getDriver().findElement(By.id("username"));
@@ -36,12 +23,24 @@ public class GroupDreamTeamTest extends BaseTest {
     }
 
     @Test
-    //test for checking merge
     public void testIna() {
         getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
         getDriver().findElement(By.id("my-text-id")).sendKeys("Hello!");
         getDriver().findElement(By.xpath("//button")).click();
         String actualResult = getDriver().findElement(By.xpath("//h1")).getText();
         Assert.assertEquals(actualResult, "Form submitted");
+    }
+
+    @Test
+    public void ElenaTest() {
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement textInput = getDriver().findElement(By.id("my-text-id"));
+        textInput.clear();
+        textInput.sendKeys("Hello everybody");
+        WebElement dropdownSelect = getDriver().findElement(By.name("my-select"));
+        Select sel = new Select(dropdownSelect);
+        sel.selectByValue("1");
+        WebElement one = getDriver().findElement(By.xpath("//option[@value='1']"));
+        Assert.assertTrue(one.isSelected());
     }
 }
