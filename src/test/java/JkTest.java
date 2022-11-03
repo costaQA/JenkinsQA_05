f19c4e41081996dc69629b321a5f27214a8367b8
Julykan <julykan777@gmail.com>
2022-11-03 17:51:46 -0500

    update test

--- src/test/java/JkTest.java
+++ src/test/java/JkTest.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -10,8 +9,7 @@ import runner.BaseTest;
 public class JkTest extends BaseTest {
         @Test
         public void testManageCookies_MainPage() throws InterruptedException {
-            System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
-            WebDriver driver = new ChromeDriver();
+            WebDriver driver = getDriver();
 
             String url = "https://openweathermap.org/";
 
