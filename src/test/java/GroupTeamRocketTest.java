9fce057c74214df2cbcc893afe0e9454b14db4bc
RomanSaf <romansafarin@gmail.com>
2022-11-02 18:06:18 -0700

    add new test testLAtimes_RomanS

--- src/test/java/GroupTeamRocketTest.java
+++ src/test/java/GroupTeamRocketTest.java
@@ -45,4 +45,12 @@ public class GroupTeamRocketTest extends BaseTest {
         getDriver().findElement (By.id ("shopping_cart_container")).click ();
         Assert.assertTrue (getDriver().findElement (By.id ("item_4_title_link")).isDisplayed ());
     }
+    @Test
+    public void testLAtimes_RomanS() throws InterruptedException{
+        getDriver().get("https://www.latimes.com/");
+        getDriver().findElement(By.xpath("//body[@class='page-body']/ps-header[@class='page-header-custom-element sticky-top']//span[@class='label']")).click();
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//ps-header/div[1]/div[1]/div[1]/nav[1]/ul[1]/li[6]/div[1]/div[1]/a[1]")).click();
+        Assert.assertEquals(getDriver().getTitle(), "Food - Los Angeles Times");
+    }
 }
