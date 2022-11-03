d861a51e44debe0018bf2c51db155eeb6e0bc1da
ViktoriiaKarpus <103855583+ViktoriiaKarpus@users.noreply.github.com>
2022-11-03 21:53:52 +0100

    testParagraph (#117)

--- src/test/java/PLGroupTest.java
+++ src/test/java/PLGroupTest.java
@@ -1,5 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class PLGroupTest extends BaseTest {
 
+    @Test
+    public void testParagraph(){
+        getDriver().get("https://www.selenium.dev/");
+
+        WebElement link = getDriver().findElement(
+                By.xpath("//div//p[text()='What you do with that power is entirely up to you.']")
+        );
+
+        Assert.assertEquals(link.getText(), "What you do with that power is entirely up to you.");
+    }
 }
