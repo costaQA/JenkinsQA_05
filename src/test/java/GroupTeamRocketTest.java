bea9d404a5759e1b82f82332cb5d7a9542a4aecf
TatianaTomilina <tatianagtomilina@gmail.com>
2022-11-03 08:20:16 -0700

    remove Thread.sleep

--- src/test/java/GroupTeamRocketTest.java
+++ src/test/java/GroupTeamRocketTest.java
@@ -131,7 +131,6 @@ public class GroupTeamRocketTest extends BaseTest {
     public void testAddToCartButton() throws InterruptedException{
         getDriver().get("https://www.demoblaze.com");
         getDriver().findElement(By.xpath("//body/div[5]/div/div[1]/div/a[4]")).click();
-        Thread.sleep(2000);
         getDriver().findElement(By.xpath("//body/div[5]/div/div[2]/div/div[1]/div/div/h4/a")).click();
         getDriver().findElement(By.xpath("//body/div[5]/div/div[2]/div[2]/div/a")).click();
         Assert.assertTrue(getDriver().findElement(By.xpath("//body/div[5]/div/div[2]/div[2]/div/a")).isDisplayed ());
