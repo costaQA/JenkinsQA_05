490a7e1356d144be0b30c655e666f86a219e15b2
ServachakM <102581167+ServachakM@users.noreply.github.com>
2022-11-04 03:53:30 +0200

    Servachak test added (#125)
    * New test by Servachak added
    
    * New test by Servachak added

--- src/test/java/CleanCodeTest.java
+++ src/test/java/CleanCodeTest.java
@@ -15,5 +15,16 @@ public class CleanCodeTest extends BaseTest {
         Assert.assertEquals(link.getText(), "ritual scepter");
     }
 
+    @Test
+    public void testBox24Menu() {
+        final String URL = "https://box24.com.ua/";
+        int expectedNumbersMenu = 11;
 
+        getDriver().get(URL);
+
+        int actualNumbersMenu = getDriver().findElements(
+                By.cssSelector(".products-menu__title-link")).size();
+
+        Assert.assertEquals(actualNumbersMenu, expectedNumbersMenu);
+    }
 }
