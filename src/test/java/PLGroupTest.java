d3aa8ab874be759153d588ac6daae0ad2fe7476a
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-11-03 20:03:46 -0600

    first test (#127)

--- src/test/java/PLGroupTest.java
+++ src/test/java/PLGroupTest.java
@@ -4,9 +4,35 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.List;
+
 public class PLGroupTest extends BaseTest {
 
     @Test
+    public void testH2HeaderSeleniumInteractionsWindows(){
+        getDriver().get("https://www.selenium.dev/documentation/webdriver/interactions/windows/");
+
+        String expectedResult = "Working with windows and tabs";
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//main/div/h1")).getText(),expectedResult);
+    }
+
+    @Test void testNavTest_Main(){
+        getDriver().get("https://www.selenium.dev/documentation/webdriver/interactions/windows/");
+
+        String expectedResult = "Java".concat("Python");
+
+        List<WebElement> countTableColumns = getDriver().findElements(By.xpath("//main//ul[@id='tabs-0']/li"));
+
+        String columnNames = "";
+        for (WebElement name : countTableColumns) {
+            columnNames += name.getText();
+        }
+
+        Assert.assertTrue(columnNames.contains(expectedResult));
+        Assert.assertEquals(countTableColumns.size(), 6);
+        }
+
     public void testParagraph(){
         getDriver().get("https://www.selenium.dev/");
 
